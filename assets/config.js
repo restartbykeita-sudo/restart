d250a0160dc0ba48cb2f5a31d298b07765809d5d
restartbykeita-sudo <restartbykeita@gmail.com>
2026-07-20 04:58:00 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 window.TRAIL_SCAN_CONFIG = Object.freeze({
   APP_NAME: 'Trail Scan',
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
+  SUPABASE_URL: 'https://tnvwdseomwzosjeemapd.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRudndkc2VvbXd6b3NqZWVtYXBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyNzU0NTUsImV4cCI6MjA5OTg1MTQ1NX0.vzfpVAn7iQlcs3Opi1TcUzYVeitN4j9Rzv9AfhuL93k',
   DEFAULT_TIMEZONE: 'Asia/Bangkok',
   SCANNER: { FPS: 18, DUPLICATE_LOCK_MS: 4000, RESULT_MS: 1200, BATCH_SIZE: 30 }
 });
